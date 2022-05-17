e0c3763d7cac3d71c496ab8079e844c25c39982c
Benjymack <newzealandbenjy@gmail.com>
2022-05-17 21:48:04 +1200

    Added argument parsing for running single files and servers Improved code quality

--- main.py
+++ main.py
@@ -1,30 +1,76 @@
 # Imports
 from flask import Flask, request
 from pyhp import load_file, run_parsed_code
+from argparse import ArgumentParser
+import os
 
 
-# Constants
-DEBUG = True
+def setup_file_action(subparser):
+    file = subparser.add_parser('file', help='Run a file')
+
+    file.add_argument('file', help='File to run')
 
 
-if __name__ == '__main__':
-    # Setup the flask application
+def setup_server_action(subparser):
+    server = subparser.add_parser('server', help='Start a server')
+
+    server.add_argument('directory', help='Directory to serve')
+    server.add_argument('-p', '--port', help='Port to serve on', type=int,
+                        default=5000)
+
+
+def setup_argument_parsing() -> ArgumentParser:
+    parser = ArgumentParser(description='Python Hypertext Preprocessor (PyHP)')
+    parser.add_argument('-d', '--debug', action='store_true', help='Debug mode',
+                        default=False)
+
+    subparser = parser.add_subparsers(dest='action', help='Action to perform',
+                                      required=True)
+    setup_file_action(subparser)
+    setup_server_action(subparser)
+
+    return parser
+
+
+def run_file():
+    parsed_code = load_file(args.file)
+    output = run_parsed_code(parsed_code, debug=args.debug)
+    print(output)
+
+
+def get_page_or_404(path: str, predefined: dict, debug: bool) -> (str, int):
+    try:
+        dom = load_file(path)
+        return run_parsed_code(dom, predefined, debug)
+    except FileNotFoundError:
+        return "File not found", 404
+
+
+def run_server():
     app = Flask(__name__)
 
     @app.route('/', defaults={'path': 'index'})
     @app.route('/<path:path>')
     def catch_all(path):
-        # Get information for initial_globals variables
         predefined = {
             '_COOKIE': request.cookies,
             '_GET': request.args,
             '_POST': request.form,
         }
 
-        try:
-            parsed_code = load_file(path)
-        except FileNotFoundError:
-            return 'File not found', 404
-        return run_parsed_code(parsed_code, predefined, DEBUG)
+        path = os.path.join(args.directory, path)
 
-    app.run()
+        return get_page_or_404(path, predefined, args.debug)
+
+    app.run(port=args.port, debug=args.debug)
+
+
+if __name__ == '__main__':
+    parser = setup_argument_parsing()
+
+    args = parser.parse_args()
+
+    if args.action == 'file':
+        run_file()
+    elif args.action == 'server':
+        run_server()
--- pyhp.py
+++ pyhp.py
@@ -7,12 +7,9 @@ from contextlib import redirect_stdout
 from copy import deepcopy
 
 import traceback
-import sys
 import os
 
-
-__all__ = ['load_file', 'run_code_block', 'run_parsed_code', 'include']
-
+__all__ = ['load_file', 'run_code_text', 'run_parsed_code', 'include']
 
 # Constants
 TAG_NAME = 'pyhp'
@@ -21,79 +18,104 @@ REMOVE_INITIAL_INDENTATION = True
 
 
 # Functions
-def load_file(file_path: str) -> BeautifulSoup:
+def get_pyhp_file_path(path: str) -> str:
+    if os.path.exists(path):
+        return path
+    else:
+        possible_path = f'{path}.{FILE_EXTENSION}'
+        if os.path.exists(possible_path):
+            return possible_path
+        else:
+            raise FileNotFoundError(f'File not found: {path}')
+
+
+def load_file(path: str) -> BeautifulSoup:
     """
     Loads a pphp file and parses it, returning the BeautifulSoup object.
     """
 
-    # Check if the pyhp file with that name exists
-    if not os.path.exists(file_path):
-        possible_path = f'{file_path}.{FILE_EXTENSION}'
-        if os.path.exists(possible_path):
-            file_path = possible_path
-        else:
-            raise FileNotFoundError(f'File not found: {file_path}')
+    file_path = get_pyhp_file_path(path)
 
-    # Open and parse the file
     with open(file_path, 'r') as f:
         return BeautifulSoup(f.read(), 'html.parser')
 
 
-def run_code_block(code_block: str,
-                   globals_: dict,
-                   locals_: dict) -> (bool, str):
+def run_code_text(code_block: str,
+                  globals_: dict,
+                  locals_: dict) -> (bool, str):
     """
     Runs the code block, in the given globals and locals environment.
     Returns a tuple of (success, output).
     """
-    output = StringIO()
+    output_text = StringIO()
     exception = None
 
     # Run the code
     try:
-        with redirect_stdout(output):
+        with redirect_stdout(output_text):
             exec(code_block, globals_, locals_)
+
+        success = True
+        output = output_text.getvalue()
     except Exception:
-        exception = traceback.format_exc()
+        success = False
+        output = f'<pre>{traceback.format_exc()}</pre>'
 
-    # Return the output of the code
-    if exception is None:
-        return True, output.getvalue()
-    else:
-        return False, f'<pre>{exception}</pre>'
+    return success, output
 
 
-def run_parsed_code(parsed_code: BeautifulSoup,
-                    initial_globals: Optional[dict] = None,
-                    debug: bool = False) -> str:
+def remove_initial_indentation(code: str) -> str:
+    lines = code.split('\n')
+    whitespace_characters = len(lines[0]) - len(lines[0].lstrip())
+    # TODO: Check if there isn't enough whitespace
+    #  (if we are removing actual characters)
+    return '\n'.join(line[whitespace_characters:] for line in lines)
+
+
+def prepare_code_text(code_block) -> str:
     """
-    Runs the parsed pyhp code, returning the html output.
+    Prepares a code block for execution.
     """
-    output_code = deepcopy(parsed_code)
+    code_text = code_block.decode_contents()
+    # TODO: Check that stripping doesn't cause issues
+    code_text = code_text.lstrip('\n').rstrip('\n')
 
+    if REMOVE_INITIAL_INDENTATION:
+        code_text = remove_initial_indentation(code_text)
+
+    return code_text
+
+
+def prepare_globals_locals(initial_globals: Optional[dict] = None)\
+        -> (dict, dict):
+    """
+    Prepares the globals and locals for execution.
+    """
     if initial_globals is None:
         globals_ = {}
     else:
         globals_ = initial_globals
     locals_ = {}
 
-    code_blocks = output_code.select(f'{TAG_NAME}:not({TAG_NAME} *)')
+    return globals_, locals_
+
+
+def run_parsed_code(dom: BeautifulSoup,
+                    initial_globals: Optional[dict] = None,
+                    debug: bool = False) -> str:
+    """
+    Runs the parsed pyhp code, returning the html output.
+    """
+    output_dom = deepcopy(dom)
+
+    globals_, locals_ = prepare_globals_locals(initial_globals)
+
+    code_blocks = output_dom.select(f'{TAG_NAME}:not({TAG_NAME} *)')
 
     for code_block in code_blocks:
-        code_text = code_block.decode_contents()
-        # TODO: Check that stripping doesn't cause issues
-        code_text = code_text.lstrip('\n').rstrip('\n')
+        code_text = prepare_code_text(code_block)
 
-        if REMOVE_INITIAL_INDENTATION:
-            lines = code_text.split('\n')
-            whitespace_characters = len(lines[0]) - len(lines[0].lstrip())
-            # TODO: Check if there isn't enough whitespace
-            #  (if we are removing actual characters)
-            code_text = '\n'.join(line[whitespace_characters:]
-                                  for line in lines)
-
-        success, output = run_code_block(code_text,
-                                         globals_, locals_)
+        success, output = run_code_text(code_text, globals_, locals_)
 
         if not success and not debug:
             raise RuntimeError(output)
@@ -103,7 +125,7 @@ def run_parsed_code(parsed_code: BeautifulSoup,
         if not success:
             break
 
-    return str(output_code)
+    return str(output_dom)
 
 
 def include(file_path: str):
