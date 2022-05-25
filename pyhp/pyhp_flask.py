8bc322d5150bbd35007a381bd64497197f85a5b5
Benjymack <newzealandbenjy@gmail.com>
2022-05-25 18:46:42 +1200

    Added ability to POST to pyhp files. Improved variable naming. Added 200 status code to normal behaviour

--- pyhp/pyhp_flask.py
+++ pyhp/pyhp_flask.py
@@ -10,20 +10,20 @@ except ImportError:
     from .file_processing import get_directory, get_absolute_path
 
 
-def create_app(directory: str) -> Flask:
-    directory = os.path.abspath(directory)  # TODO: Move into separate function
+def create_app(base_dir: str) -> Flask:
+    base_dir = os.path.abspath(base_dir)  # TODO: Move into separate function
 
     app = Flask(__name__)
 
-    @app.route('/', defaults={'path': 'index'})
-    @app.route('/<path:path>')
-    def catch_all(path):
-        pyhp_class = Pyhp(directory, app.config['DEBUG'],
+    @app.route('/', defaults={'path': 'index'}, methods=['GET', 'POST'])
+    @app.route('/<path:path>', methods=['GET', 'POST'])
+    def catch_all(path) -> Response:
+        pyhp_class = Pyhp(base_dir, app.config['DEBUG'],
                           dict(request.cookies),
                           dict(request.args),
                           dict(request.form))
 
-        absolute_path = get_absolute_path(path, directory)
+        absolute_path = get_absolute_path(path, base_dir)
 
         return get_page_or_404(absolute_path, pyhp_class, app.config['DEBUG'])
 
@@ -34,7 +34,7 @@ def get_page_or_404(absolute_path: str, pyhp_class: Pyhp,
                     debug: bool) -> (str, int):
     try:
         dom = load_file(absolute_path)
-        return run_parsed_code(dom, pyhp_class)
+        return run_parsed_code(dom, pyhp_class), 200
     except FileNotFoundError:
         print(os.getcwd())
         if debug:
