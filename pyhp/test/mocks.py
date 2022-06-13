1094414c3d15003970e7a02b7a636ed60fbf00ef
Benjymack <newzealandbenjy@gmail.com>
2022-06-13 21:25:02 +1200

    Added directory and greater file support to MockFileProcessor, to allow testing of directories.

--- pyhp/test/mocks.py
+++ pyhp/test/mocks.py
@@ -2,7 +2,7 @@
 Sets up mock classes for the testing of the PyHP framework.
 """
 
-from typing import Union
+from typing import Union, Optional
 from pathlib import PurePath
 
 from pyhp.file_processing import FileProcessor
@@ -14,8 +14,10 @@ class MockFileProcessor(FileProcessor):
     file paths and contents, or a string, and returns the contents of the file
     at the given path.
     """
-    def __init__(self, file_contents: Union[dict[PurePath, str], str] = ''):
+    def __init__(self, file_contents: Union[dict[PurePath, str], str] = '',
+                 directories: Optional[set[PurePath]] = None):
         self._file_contents = file_contents
+        self._directories: set[PurePath] = directories or []
 
     def get_file_contents(self, path: PurePath) -> str:
         if isinstance(self._file_contents, dict):
@@ -23,10 +25,10 @@ class MockFileProcessor(FileProcessor):
         return self._file_contents
 
     def is_dir(self, path: PurePath) -> bool:
-        raise NotImplementedError('Please implement is_dir for specific test.')
+        return path in self._directories
 
     def is_file(self, path: PurePath) -> bool:
-        raise NotImplementedError('Please implement is_file for specific test.')
+        return path in self._file_contents
 
     def is_pyhp_file(self, path: PurePath) -> bool:
         raise NotImplementedError(
