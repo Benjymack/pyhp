fac299988b4556f5825d7de2c6f0f2f49c86f702
Benjymack <newzealandbenjy@gmail.com>
2022-06-20 18:42:32 +1200

    Added file and server options to command line interface

---
+++ src/pyhp/__main__.py
@@ -0,0 +1,45 @@
+"""
+PyHP: Python Hypertext Preprocessor
+"""
+
+from argparse import ArgumentParser
+from pathlib import PurePath, Path
+
+try:
+    from pyhp_interface import RootPyhp
+    from file_processing import SystemFileProcessor
+    from pyhp_flask import create_app
+except ImportError:
+    from .pyhp_interface import RootPyhp
+    from .file_processing import SystemFileProcessor
+    from .pyhp_flask import create_app
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Python Hypertext Preprocessor (PyHP)')
+    parser.add_argument('-d', '--debug', action='store_true', help='Debug mode',
+                        default=False)
+
+    action_parser = parser.add_subparsers(dest='action',
+                                          help='Action to perform',
+                                          required=True)
+    file_parser = action_parser.add_parser('file', help='Run a file')
+    file_parser.add_argument('file', help='File to run')
+
+    server_parser = action_parser.add_parser('server', help='Run a server')
+    server_parser.add_argument('directory', help='Directory to serve')
+    server_parser.add_argument('--port', help='Port to serve on', type=int,
+                               default=5000)
+
+    args = parser.parse_args()
+
+    if args.action == 'file':
+        base_dir = Path(args.file).parent.absolute()
+        root_pyhp = RootPyhp(base_dir, SystemFileProcessor(base_dir),
+                             args.debug)
+        print(root_pyhp.run_file(PurePath(PurePath(args.file).name)))
+    elif args.action == 'server':
+        app = create_app(args.directory)
+        app.run(port=args.port, debug=args.debug)
+    else:
+        parser.print_help()
