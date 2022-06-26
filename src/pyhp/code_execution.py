ac41d486100eb9a3795fca2ca167e465f97c95b0
Benjymack <newzealandbenjy@gmail.com>
2022-06-26 14:22:39 +1200

    Moved context into PyhpInterface, rather than code execution

--- src/pyhp/code_execution.py
+++ src/pyhp/code_execution.py
@@ -9,7 +9,6 @@ from contextlib import redirect_stdout
 from copy import deepcopy
 from traceback import format_exc
 from typing import TYPE_CHECKING, Any
-import sys
 
 from bs4 import BeautifulSoup, Tag
 
@@ -27,15 +26,13 @@ if TYPE_CHECKING:
 
 
 def run_parsed_code(dom: BeautifulSoup,
-                    pyhp_class: 'Pyhp',
-                    file_processor: FileProcessor) -> str:
+                    pyhp_class: 'Pyhp') -> str:
     output_dom = deepcopy(dom)
     code_blocks = get_code_blocks(output_dom)
 
-    globals_, locals_ = prepare_context(pyhp_class, file_processor)
-
     for code_block in code_blocks:
-        success = run_code_block(code_block, globals_, locals_, pyhp_class)
+        success = run_code_block(code_block, pyhp_class.globals,
+                                 pyhp_class.locals, pyhp_class)
 
         if not success:
             break
@@ -73,18 +70,3 @@ def run_code_text(code_text: str,
         output = output_text.getvalue()
 
     return success, output
-
-
-def prepare_context(pyhp_class: 'Pyhp',
-                    file_processor: FileProcessor) -> (dict[str, Any],
-                                                       dict[str, Any]):
-    context_globals = {'pyhp': pyhp_class}
-    context_locals = {}
-
-    # TODO: This is potentially a security flaw
-    current_dir = str(file_processor.get_absolute_path(pyhp_class.current_dir))
-
-    if current_dir not in sys.path:
-        sys.path.append(current_dir)
-
-    return context_globals, context_locals
--- src/pyhp/pyhp_interface.py
+++ src/pyhp/pyhp_interface.py
@@ -2,10 +2,11 @@
 Interface for the PyHP programs.
 """
 
-from typing import Optional
+from typing import Optional, Any
 from pathlib import PurePath
 from bs4 import BeautifulSoup
 import markupsafe
+import sys
 
 try:
     from file_processing import FileProcessor
@@ -48,6 +49,8 @@ class Pyhp:
 
         self._redirect_info: Optional[(str, int)] = None
 
+        self.globals, self.locals = self._prepare_context()
+
     def display(self, relative_path: str):
         """Include another pyhp file and print it."""
         print(self.include(relative_path), end='')
@@ -72,8 +75,20 @@ class Pyhp:
         return run_parsed_code(
             self._parse_file(PurePath(relative_path)),
             self,
-            self._file_processor,
         )
+
+    def _prepare_context(self) -> (dict[str, Any], dict[str, Any]):
+        context_globals = {'pyhp': self}
+        context_locals = {}
+
+        # TODO: This is potentially a security flaw
+        current_dir = str(
+            self._file_processor.get_absolute_path(self.current_dir))
+
+        if current_dir not in sys.path:
+            sys.path.append(current_dir)
+
+        return context_globals, context_locals
 
     def _parse_file(self, relative_path: PurePath) -> BeautifulSoup:
         return parse_text(self._load_file(relative_path))
